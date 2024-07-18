33494233f7939931d0ca0b2af50f898842c9735e
Tobias <tobias_weise@gmx.de>
2024-07-18 20:58:53 +0200

    added examples

--- viz_widget.js
+++ viz_widget.js
@@ -4,14 +4,10 @@ class DotGraphElement extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: 'open'});
-
         this.content_div = document.createElement('div');
         this.shadowRoot.appendChild(this.content_div);
-
         this.slot_ele = document.createElement('slot');
-
         this.shadowRoot.appendChild(this.slot_ele);
-
         this.dot_text = "";
     }
 
@@ -19,20 +15,15 @@ class DotGraphElement extends HTMLElement {
         let layout = this.hasAttribute('layout') ? this.getAttribute('layout') : "fdp";
         let style = this.hasAttribute('style') ? this.getAttribute('style') : "";
         this.content_div.style = style;
-
         let that = this;
         this.slot_ele.addEventListener('slotchange', e => {
-
             let graphviz_text = that.innerText;
             this.dot_text = graphviz_text;
-
             this.content_div.innerHTML = Viz(this.dot_text, {engine:layout});
             this.slot_ele.style.display = "none";
-
             this.content_div.children[0].setAttribute("width", this.content_div.style.width);
             this.content_div.children[0].setAttribute("height", this.content_div.style.height);
         });
-
     }
 
     disconnectedCallback() {
@@ -59,10 +50,7 @@ class DotGraphElement extends HTMLElement {
 
     }
 
-
-
-
 }
 
-// Neues Element definieren
 customElements.define('dot-graph', DotGraphElement);
+
